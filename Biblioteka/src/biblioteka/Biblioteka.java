4328389901a1523bb337d772ccf39edeec4f3824
lukaobrenic <luka.obrenic.best@gmail.com>
2023-03-07 09:53:05 +0100

    Revert "Dodat komentar u klasu Biblioteka"
    This reverts commit 06c8c5701340b1156bd9747c0e3524b9de6e0b85.

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -12,7 +12,7 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	private List<Knjiga> knjige =  new ArrayList<Knjiga>();
 	
-	//Pravi komentar, pravi kod
+	
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
 		if(knjiga == null)
